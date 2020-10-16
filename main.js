e5af6aa9f31819892373d36b2322176c207074cb
rustyNick <Rusty-nick@live.com>
2020-10-16 13:02:21 +0200

    add js code for card render
    Adds card code render for JS

--- main.js
+++ main.js
@@ -28,4 +28,44 @@ function addProductsToWebpage() {
     // an explainetory comment like the one for this function, see row 22.
     
     // TODO: Remove the console.log and these comments when you've read them.
+
+    function renderPersonCard(person){
+
+        let wrapper = document.getElementById('wrapper');
+    
+        let mainContainer = document.createElement("div")
+        mainContainer.id ='mainContainer';
+    
+        let infoPerson = document.createElement('div')
+        infoPerson.style.width='100%'
+        infoPerson.style.height='400px'
+    
+        let fullname = document.createElement('h3')
+        fullname.innerText = person.firstname +' '+ person.lastname;
+    
+        let personAge = document.createElement('p')
+        personAge.innerHTML = person.age
+    
+        let buttonAdd = document.createElement('BUTTON')
+        buttonAdd.addEventListener('click', addperson)
+        buttonAdd.innerText = 'Add'
+    
+        let buttonDel = document.createElement('BUTTON')
+        buttonDel.addEventListener('click', Delperson)
+        buttonDel.innerText = 'Delete'
+    
+        infoPerson.appendChild(fullname)
+        infoPerson.appendChild(personAge)
+    
+        infoPerson.appendChild(buttonAdd)
+        infoPerson.appendChild(buttonDel)
+    
+        mainContainer.appendChild(infoPerson)
+        wrapper.appendChild(mainContainer)
+    }
+
+
 }
+
+
+
